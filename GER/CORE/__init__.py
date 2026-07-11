a1b52abd187fc3bfe10af98840313eca3c8bf5f4
eduardobatistadefreitas-art <eduardobatistadefreitas@gmail.com>
2026-07-11 17:35:01 -0300

    Create __init__.py

---
+++ GER/CORE/__init__.py
@@ -0,0 +1,12 @@
+"""
+GER CORE
+
+Pacote principal da Geometria Espectral Relacional (GER).
+
+Este pacote contém o núcleo permanente do framework,
+incluindo o motor temporal, métricas, potenciais,
+validação e demais componentes reutilizáveis.
+
+Nenhum código deve ser executado automaticamente durante
+a importação deste pacote.
+"""
