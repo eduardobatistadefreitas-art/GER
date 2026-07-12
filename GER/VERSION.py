ea93441f9e7f26237818f7e4c1aed038a27d09a7
eduardobatistadefreitas-art <eduardobatistadefreitas@gmail.com>
2026-07-11 21:32:03 -0300

    Create VERSION.py

---
+++ GER/VERSION.py
@@ -0,0 +1,20 @@
+"""
+=========================================================
+GER Framework
+
+Version information
+
+=========================================================
+"""
+
+FRAMEWORK_NAME = "GER - Geometria Espectral Relacional"
+
+FRAMEWORK_VERSION = "1.0"
+
+SCIENTIFIC_PHASE = "S26-B2"
+
+STATUS = "Development"
+
+AUTHOR = "Eduardo Batista de Freitas"
+
+LAST_UPDATE = "2026-07-11"
