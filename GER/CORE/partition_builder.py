e290f41d49da606db87cc9552a5de5e58ba0c9ab
eduardobatistadefreitas-art <eduardobatistadefreitas@gmail.com>
2026-07-14 20:04:26 -0300

    Update partition_builder.py

--- GER/CORE/partition_builder.py
+++ GER/CORE/partition_builder.py
@@ -1,1 +1,71 @@
+"""
+============================================================
+GER
 
+Partition Builder
+
+S27 Core
+
+Constrói uma partição a partir de uma aplicação.
+
+A teoria da S27 demonstra que toda aplicação
+
+    f : X -> Y
+
+induz naturalmente uma partição sobre X.
+
+Este módulo implementa exatamente essa construção.
+
+============================================================
+"""
+
+from collections import defaultdict
+
+from GER.CORE.partition import Partition
+
+
+# ------------------------------------------------------------
+# Builder
+# ------------------------------------------------------------
+
+def build_partition(
+    universe,
+    key,
+):
+    """
+    Constrói a partição induzida por uma aplicação.
+
+    Parameters
+    ----------
+    universe : iterable
+
+        Conjunto de elementos.
+
+    key : callable
+
+        Aplicação
+
+            key(element)
+
+        que retorna o valor observacional utilizado
+        para definir as classes.
+
+    Returns
+    -------
+    Partition
+    """
+
+    groups = defaultdict(list)
+
+    for element in universe:
+
+        value = key(element)
+
+        groups[value].append(element)
+
+    blocks = list(groups.values())
+
+    return Partition(
+        universe=universe,
+        blocks=blocks,
+    )
