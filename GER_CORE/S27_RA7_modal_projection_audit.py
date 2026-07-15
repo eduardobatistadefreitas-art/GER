2190b300f7cc8cb6fed3532b80072e99675a0976
eduardobatistadefreitas-art <eduardobatistadefreitas@gmail.com>
2026-07-15 11:07:42 -0300

    Create S27_RA7_modal_projection_audit.py

---
+++ GER_CORE/S27_RA7_modal_projection_audit.py
@@ -0,0 +1,10 @@
+import inspect
+
+from GER.CORE.ger_modal import (
+    modal_projection,
+    modal_probability,
+)
+
+print(inspect.getsource(modal_projection))
+print()
+print(inspect.getsource(modal_probability))
