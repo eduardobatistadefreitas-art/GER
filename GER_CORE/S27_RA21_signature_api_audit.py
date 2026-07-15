f01d593f245dc618c9421fb1f8f953988fd3356e
eduardobatistadefreitas-art <eduardobatistadefreitas@gmail.com>
2026-07-15 15:00:00 -0300

    Create S27_RA21_signature_api_audit.py

---
+++ GER_CORE/S27_RA21_signature_api_audit.py
@@ -0,0 +1,15 @@
+import inspect
+
+from GER.CORE.signature_api import Signature
+
+print("=" * 60)
+print("GER")
+print("S27-RA21")
+print("Signature API Audit")
+print("=" * 60)
+print()
+
+print(Signature)
+print()
+
+print(inspect.getsource(Signature))
