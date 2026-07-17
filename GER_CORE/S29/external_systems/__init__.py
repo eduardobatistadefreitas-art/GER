0452bffba28275a1d29d61cdef72f408c43b34b4
eduardobatistadefreitas-art <eduardobatistadefreitas@gmail.com>
2026-07-17 14:07:40 -0300

    Update __init__.py

--- GER_CORE/S29/external_systems/__init__.py
+++ GER_CORE/S29/external_systems/__init__.py
@@ -1,1 +1,18 @@
+"""
+=========================================================
+GER
+S29
 
+External Systems Package
+=========================================================
+"""
+
+from .duffing import simulate_duffing
+from .harmonic import simulate_harmonic
+from .white_noise import simulate_white_noise
+
+__all__ = [
+    "simulate_duffing",
+    "simulate_harmonic",
+    "simulate_white_noise",
+]
