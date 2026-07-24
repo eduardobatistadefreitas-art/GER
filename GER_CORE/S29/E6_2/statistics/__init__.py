163ab4c970d35a6192a84d42bf47d1afb8097dee
eduardobatistadefreitas-art <eduardobatistadefreitas@gmail.com>
2026-07-23 22:43:53 -0300

    Update __init__.py

--- GER_CORE/S29/E6_2/statistics/__init__.py
+++ GER_CORE/S29/E6_2/statistics/__init__.py
@@ -1,1 +1,27 @@
+"""
+============================================================
+GER
+Statistics Library
+============================================================
 
+Reusable statistical tools used by GER observatories.
+
+Modules
+-------
+descriptive
+distribution_fitting
+hypothesis_tests
+information
+information_criteria
+spectrum
+============================================================
+"""
+
+from .descriptive import *
+from .distribution_fitting import *
+from .hypothesis_tests import *
+from .information import *
+from .information_criteria import *
+from .spectrum import *
+
+STATISTICS_VERSION = "2.0"
