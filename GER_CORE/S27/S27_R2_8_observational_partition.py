4ad85bb554415c139b8ba65a34cd2347f18a4a65
eduardobatistadefreitas-art <eduardobatistadefreitas@gmail.com>
2026-07-27 20:08:22 -0300

    Update S27_R2_8_observational_partition.py

--- GER_CORE/S27/S27_R2_8_observational_partition.py
+++ GER_CORE/S27/S27_R2_8_observational_partition.py
@@ -22,11 +22,11 @@ from GER.CORE.ger_observational_snapshot import (
     build_observational_snapshot,
 )
 
-from GER_CORE.S26_B35_persistence_metrics import (
+from GER_CORE.S26.S26_B35_persistence_metrics import (
     run_persistence_observatory,
 )
 
-from GER_CORE.S26_B36_geometry_scan import (
+from GER_CORE.S26.S26_B36_geometry_scan import (
     run_geometry_scan,
     build_trajectory,
     compute_confinement,
