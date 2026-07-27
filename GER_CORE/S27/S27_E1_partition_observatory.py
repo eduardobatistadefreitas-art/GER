1d83a28f8244afb506a1b03e09a0af1bfa061db7
eduardobatistadefreitas-art <eduardobatistadefreitas@gmail.com>
2026-07-27 19:39:06 -0300

    Update S27_E1_partition_observatory.py

--- GER_CORE/S27/S27_E1_partition_observatory.py
+++ GER_CORE/S27/S27_E1_partition_observatory.py
@@ -26,7 +26,7 @@ from GER.CORE.partition_builder import build_partition
 
 from GER.CORE.partition_metrics import summary
 
-from GER_CORE.S26_B36_geometry_scan import (
+from GER_CORE.S26.S26_B36_geometry_scan import (
     run_geometry_scan,
 )
 
