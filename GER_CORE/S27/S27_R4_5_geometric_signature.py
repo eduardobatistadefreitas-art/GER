32ec8550897a311eefd4a2634cbf0f1d8065df56
eduardobatistadefreitas-art <eduardobatistadefreitas@gmail.com>
2026-07-27 20:16:19 -0300

    Update S27_R4_5_geometric_signature.py

--- GER_CORE/S27/S27_R4_5_geometric_signature.py
+++ GER_CORE/S27/S27_R4_5_geometric_signature.py
@@ -11,7 +11,7 @@
 
 import numpy as np
 
-from GER_CORE.S27_R4_1_logistic_state import (
+from GER_CORE.S27.S27_R4_1_logistic_state import (
     generate_logistic_state,
 )
 
@@ -19,11 +19,11 @@ from GER.CORE.ger_observational_snapshot import (
     build_observational_snapshot,
 )
 
-from GER_CORE.S26_B35_persistence_metrics import (
+from GER_CORE.S26.S26_B35_persistence_metrics import (
     run_persistence_observatory,
 )
 
-from GER_CORE.S26_B36_geometry_scan import (
+from GER_CORE.S26.S26_B36_geometry_scan import (
 
     build_trajectory,
 
