e95f6046020a960ea090a23dd028be481b4d0c45
eduardobatistadefreitas-art <eduardobatistadefreitas@gmail.com>
2026-07-27 19:58:08 -0300

    Update S27_R1_4_persistence_observation.py

--- GER_CORE/S27/S27_R1_4_persistence_observation.py
+++ GER_CORE/S27/S27_R1_4_persistence_observation.py
@@ -15,7 +15,7 @@ from GER.CORE.ger_observational_snapshot import (
     build_observational_snapshot,
 )
 
-from GER_CORE.S26_B35_persistence_metrics import (
+from GER_CORE.S26.S26_B35_persistence_metrics import (
     run_persistence_observatory,
 )
 
