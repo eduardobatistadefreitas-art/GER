4136058414300acd8c7d052d6089e7f108dd44e6
eduardobatistadefreitas-art <eduardobatistadefreitas@gmail.com>
2026-07-27 20:07:23 -0300

    Update S27_R2_6_structural_certificate.py

--- GER_CORE/S27/S27_R2_6_structural_certificate.py
+++ GER_CORE/S27/S27_R2_6_structural_certificate.py
@@ -19,11 +19,11 @@ from GER.CORE.ger_observational_snapshot import (
     build_observational_snapshot,
 )
 
-from GER_CORE.S26_B35_persistence_metrics import (
+from GER_CORE.S26.S26_B35_persistence_metrics import (
     run_persistence_observatory,
 )
 
-from GER_CORE.S26_B36_geometry_scan import (
+from GER_CORE.S26.S26_B36_geometry_scan import (
 
     build_trajectory,
 
@@ -37,7 +37,7 @@ from GER_CORE.S26_B36_geometry_scan import (
 
 )
 
-from GER_CORE.S26_B36_stationary_scan import (
+from GER_CORE.S26.S26_B36_stationary_scan import (
     stationary_scan,
 )
 
