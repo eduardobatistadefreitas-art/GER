eb2b1f0fed8eb71865795cdf8078843f07c6a350
eduardobatistadefreitas-art <eduardobatistadefreitas@gmail.com>
2026-07-27 20:34:09 -0300

    Update S27_R5_4_persistence_observation.py

--- GER_CORE/S27/S27_R5_4_persistence_observation.py
+++ GER_CORE/S27/S27_R5_4_persistence_observation.py
@@ -11,7 +11,7 @@
 
 import numpy as np
 
-from GER_CORE.S27_R5_1_lorenz_state import (
+from GER_CORE.S27.S27_R5_1_lorenz_state import (
     generate_lorenz_state,
 )
 
@@ -19,7 +19,7 @@ from GER.CORE.ger_observational_snapshot import (
     build_observational_snapshot,
 )
 
-from GER_CORE.S26_B35_persistence_metrics import (
+from GER_CORE.S26.S26_B35_persistence_metrics import (
     run_persistence_observatory,
 )
 
