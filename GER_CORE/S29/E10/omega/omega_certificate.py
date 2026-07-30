242340ecd4227389c2bec623ae6cad563bdc8304
eduardobatistadefreitas-art <eduardobatistadefreitas@gmail.com>
2026-07-30 16:02:07 -0300

    Create omega_certificate.py

---
+++ GER_CORE/S29/E10/omega/omega_certificate.py
@@ -0,0 +1,14 @@
+"""
+=============================================================
+OMEGA CERTIFICATE
+=============================================================
+"""
+
+from __future__ import annotations
+
+
+def build_certificate(generator):
+
+    return {
+        "generator": generator.name,
+    }
